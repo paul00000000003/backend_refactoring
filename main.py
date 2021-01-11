c61008fd2af6159b3d0bd14b12b235d1973aa422
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2021-01-11 16:44:29 +0100

    master

--- main.py
+++ main.py
@@ -2,81 +2,80 @@ __winc_id__ = '9920545368b24a06babf1b57cee44171'
 __human_name__ = 'refactoring'
 
 
-electricians = []
-painters = []
-plumbers = []
+class electrician:
+    def __init__(self, name, price):
+        self.name = name
+        if "electrician" in specialists:
+            specialists["electrician"].append({"name": name, "price": price})
+        else:
+            specialists["electrician"] = [{"name": name, "price": price}]
 
-class Specialists:
-    class electrician:
-        def __init__(self, name, price):
-            self.name = name
-            if name in electricians:
-                None
-            else:
-                electricians.append({"name": name, "price": price})
 
-    class painter:
-        def __init__(self, name, price):
-            self.name = name
-            if name in painters:
-                None
-            else:
-                painters.append({"name": name, "price": price})
+class painter:
+    def __init__(self, name, price):
+        self.name = name
+        if "painter" in specialists:
+            specialists["painter"].append({"name": name, "price": price})
+        else:
+            specialists["painter"] = [{"name": name, "price": price}]
 
-    class plumber:
-        def __init__(self, name, price):
-            self.name = name
-            if name in plumbers:
-                None
-            else:
-                plumbers.append({"name": name, "price": price})
+
+class plumber:
+    def __init__(self, name, price):
+        self.name = name
+        if "plumber" in specialists:
+            specialists["plumber"].append({"name": name, "price": price})
+        else:
+            specialists["plumber"] = [{"name": name, "price": price}]
+
+
+def get_cheapest_specialist(needed_people):
+    indexPreferred = -1
+    min_price = 0
+    for professional_person in needed_people:
+        if indexPreferred == -1:
+            indexPreferred = 0
+            min_price = professional_person['price']
+        else:
+            if professional_person['price'] < min_price:
+                indexPreferred = needed_people.index(professional_person)
+    return
 
 
 class Homeowner:
-    def __init__(self, name, address, needs):
+    def __init__(self, name, address, needs, specialists):
         self.name = name
         self.address = address
         self.needs = needs
+        self.specialists = specialists
 
     def make_contracts(self):
         contracts = []
+        print("specialists :"+specialists.__repr__())
+        print(specialists['electrician'])
+
         for need in self.needs:
-            professions = {"electrician": electricians,
-                           "plumber": plumbers,
-                           "painter": painters}
-            professions_table = professions.get(need, "not applicable")
-            if len(professions_table) == 1:
-                contracts.append(professions_table[0]['name'])
-            else:
-                if len(professions_table) == 0:
+            if need in self.specialists:
+                needed_people = self.specialists[need]
+                if len(needed_people) == 1:
+                    contracts.append(needed_people[0]['name'])
+                elif len(needed_people) == 0:
                     print(f"Voor het beroep {need} is nog niemand opgevoerd")
                 else:
-                    indexPreferred = -1
-                    min_price = 0
-                    for profession in professions_table:
-                        if indexPreferred == -1:
-                            indexPreferred = 0
-                            min_price = profession['price']
-                        else:
-                            if profession['price'] < min_price:
-                                indexPreferred = professions_table.index(
-                                    profession)
-                    # Merk op dat index hier altijd groter of gelijk aan nul is
-                    contracts.append(professions_table[indexPreferred])
+                    desired_specialist = get_cheapest_specialist(needed_people)
+                    contracts.append(desired_specialist['name'])
+            else:
+                print(f"there's no {need}")
         return contracts
 
 
-alice = Specialists().electrician('Alice Aliceville', 0)
+specialists = {}
 
+alice = electrician('Alice Aliceville', 0)
+bob = painter('Bob Bobsville', 0)
+craig = plumber('Craig Craigsville', 0)
 
 alfred = Homeowner('Alfred Alfredson', 'Alfredslane 123',
-                   ['painter', 'plumber'])
+                   ['painter', 'plumber'], specialists)
 for contract in alfred.make_contracts():
-    print(contract)
-
-
-"""
-print("Alfred's contracts:", alfred_contracts)
-print("Bert's contracts:", bert_contracts)
-print("Candice's contracts:", candice_contracts)
-"""
+    print("wanted person : "+contract)
